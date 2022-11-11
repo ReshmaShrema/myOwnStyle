ad8b18e20b366a7b8c441b13b39a07bc53e6deef
ReshmaShrema <shremareshma@gmail.com>
2022-11-11 12:23:21 +0530

    route setting

--- app.js
+++ app.js
@@ -1,11 +1,77 @@
+// importing express module
 const express = require('express');
+
+// importing express-session module
+const session = require('express-session');
+
+// importing file-store module
+const filestore = require('session-file-store')(session);
 
 //view engine setup npm i express-handlebars
 const hbs = require("express-handlebars");
 
-//setting the server app,init express app
+//setting up the server app,init express app
 const app =express();
 
+// creating session
+app.use(
+  session({
+    name: "session-id",
+    secret: "GFGEnter", // Secret key,
+    saveUninitialized: false,
+    resave: false,
+    store: new filestore(),
+  })
+);
+
+
+// Asking for the authorization
+function auth(req, res, next) {
+    // Checking for the session
+    console.log(req.session)
+  
+// Checking for the authorization
+    if (!req.session.user) {
+        var authHeader = req.headers.authorization;
+        console.log(authHeader);
+        var err = new Error("You are not authenticated")
+        res.setHeader("WWW-Authenticate", "Basic")
+        err.status = 401
+        next(err)
+  
+        var auth = new Buffer.from(authHeader.split(' ')[1],
+            "base64").toString().split(":")
+  
+        // Reading username and password
+        var username = auth[0]
+        var password = auth[1]
+        if (username == "admin2" && password == "password") {
+            req.session.user = "admin2"
+            next()
+        }
+        else {
+            // Retry incase of incorrect credentials
+            var err = new Error('You are not authenticated!');
+            res.setHeader("WWW-Authenticate", "Basic")
+            err.status = 401;
+            return next(err);
+        }
+    }
+    else {
+        if (req.session.user === "admin2") {
+            next()
+        }
+        else {
+            var err = new Error('You are not authenticated!');
+            res.setHeader("WWW-Authenticate", "Basic")
+            err.status = 401;
+            return next(err);
+        }
+    }
+}
+  
+// Middlewares
+app.use(auth)
 //Middleware for parsing data
 app.use(express.json());
 
@@ -13,7 +79,7 @@ app.use(express.json());
 const path = require('path');
 app.use(express.static(path.join(__dirname,'public')))
 
-// view engin
+// view engine
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','hbs')
 
@@ -27,12 +93,16 @@ app.use('/user',userRouter);
 app.use('/admin',adminRouter);
 
 
+if(process.env.NODE_ENV !== 'production'){
+    require("dotenv").config();
+}
 
     
 // port setting
-app.listen(3000,(error)=>{
+const port = process.env.PORT || 3000;
+app.listen(port,(error)=>{
     if(!error)
-        console.log("Server is Successfully Running, and app is listening on Port 3000");
+        console.log(`Server is Successfully Running, and app is listening on Port ${port}`);
     else
     console.log("Error occured,server can't start",error);
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router =express.Router()
 
+//Variable setting
+const email = process.env.ADMIN_EMAIL_ID;
+const password = process.env.ADMIN_PASSWORD;
 
+
+
+//admin signin
 router.get('/signin',(req,res)=>{
+    (req.session.adminLoggin==true)?res.redirect('/admin/dashboard'):res.render('admin/signin',)
     res.render('admin/admin-login')
 })
 
